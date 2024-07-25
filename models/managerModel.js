18a78ba069b546295f574de00fae1c2f807beecc
MdShijanAli <freelancershijan@gmail.com>
2024-07-25 23:32:02 +0600

    fix: manager module searchManager api design update with sort

--- models/managerModel.js
+++ models/managerModel.js
@@ -15,9 +15,9 @@ function managerModel() {
     connection.query(query, values, cb)
   }
 
-  const editManager = (managerData, managerId, cb) =>{
-     const query = "UPDATE `managers` SET `first_name`=?,`last_name`=?,`phone`=?,`email`=?,`photo`=?,`address`=? WHERE id = ?";
-     const values = [
+  const editManager = (managerData, managerId, cb) => {
+    const query = "UPDATE `managers` SET `first_name`=?,`last_name`=?,`phone`=?,`email`=?,`photo`=?,`address`=? WHERE id = ?";
+    const values = [
       managerData.first_name,
       managerData.last_name,
       managerData.phone,
@@ -26,13 +26,13 @@ function managerModel() {
       managerData.address,
       managerId
     ];
-     connection.query(query, values, cb)
+    connection.query(query, values, cb)
   }
 
-  const getAllManagers = (page = 1, limit =  20, sort_by = 'created_at', sort_order = 'DESC', cb) => {
+  const getAllManagers = (page = 1, limit = 20, sort_by = 'created_at', sort_order = 'DESC', cb) => {
     const skip = (page - 1) * limit;
     const totalQuery = `SELECT COUNT(*) as count FROM managers`;
-    const query = `SELECT * FROM managers ORDER BY ${ sort_by } ${ sort_order } LIMIT ${skip}, ${limit}`;
+    const query = `SELECT * FROM managers ORDER BY ${ sort_by } ${ sort_order } LIMIT ${ skip }, ${ limit }`;
     connection.query(totalQuery, (err, totalResult) => {
       if (err) {
         return cb(err);
@@ -53,26 +53,36 @@ function managerModel() {
   const getManagerById = (managerId, cb) => {
     console.log('Get manager ID', managerId);
     const query = "SELECT * FROM `managers` WHERE id = ?";
-    connection.query(query, [managerId], (err, result)=>{
-      if(err){
+    connection.query(query, [managerId], (err, result) => {
+      if (err) {
         cb(err)
-      }else{
+      } else {
         cb(null, result)
       }
     })
   }
 
-  const searchManagers = (page = 1, limit = 20, searchQuery, cb) =>{
+  const searchManagers = (page = 1, limit = 20, searchQuery, sort_by = 'created_at', sort_order = 'DESC', cb) => {
     const skip = (page - 1) * limit;
-    const query = `SELECT * FROM managers WHERE first_name LIKE '%${ searchQuery }%' OR last_name LIKE '%${ searchQuery }%' OR phone LIKE '%${ searchQuery }%' OR email LIKE '%${ searchQuery }%' OR address LIKE '%${ searchQuery }%' LIMIT ${ skip }, ${ limit }`
-    connection.query(query, (err, results)=>{
-      if(err){
-        cb(err)
-      }else{
-        cb(null, results)
+    const totalQuery = `SELECT COUNT(*) as count FROM managers WHERE first_name LIKE '%${ searchQuery }%' OR last_name LIKE '%${ searchQuery }%' OR phone LIKE '%${ searchQuery }%' OR email LIKE '%${ searchQuery }%' OR address LIKE '%${ searchQuery }%'`;
+    const query = `SELECT * FROM managers WHERE first_name LIKE '%${ searchQuery }%' OR last_name LIKE '%${ searchQuery }%' OR phone LIKE '%${ searchQuery }%' OR email LIKE '%${ searchQuery }%' OR address LIKE '%${ searchQuery }%' ORDER BY ${ sort_by } ${ sort_order } LIMIT ${ skip }, ${ limit }`
+
+    connection.query(totalQuery, (err, totalResult) => {
+      if (err) {
+        return cb(err);
       }
-    })
-  }
+
+      const total = totalResult[0].count;
+
+      connection.query(query, (err, results) => {
+        if (err) {
+          cb(err);
+        } else {
+          cb(null, { results, total });
+        }
+      });
+    });
+  };
 
   const deleteManager = (managerId, cb) => {
     const query = "DELETE FROM `managers` WHERE id = ?"
@@ -80,13 +90,13 @@ function managerModel() {
   }
 
 
-  return{
-     createManager,
-     editManager,
-     getAllManagers,
-     getManagerById,
-     searchManagers,
-     deleteManager
+  return {
+    createManager,
+    editManager,
+    getAllManagers,
+    getManagerById,
+    searchManagers,
+    deleteManager
   }
 }
 
